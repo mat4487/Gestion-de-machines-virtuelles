59eae75b42a4d389d0c707036b6f881b27d95b1e
Mathieu <mathieu@mathieu-VirtualBox.(none)>
2012-03-05 17:18:21 +0100

    ajout d'un script pour l'install de virt-manager

---
+++ Mathieu_Lamouroux/virt-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+###########################################
+## Script d'installation de virt-manager ##
+###########################################
+
+# Mise à jour du système 
+apt-get update
+apt-get upgrade -y
+
+# Installation de libvirt pour communiquer avec le démon xend
+apt-get install libvirt-bin
+
+# Activation des sockets Unix
+# (nécessaire pour la communication entre libvirt et xend)
+cat "(xend-unix-server yes)" >> /etc/xen/xend-config.sxp
+
+# Redémarrage de xend pour application des changements
+/etc/init.d/xend restart
+
+# Installation de virt-manager
+apt-get install virt-manager
+
+#############
+# Optionnel #
+#############
+# Lancement du script pour le déploiement de 7 machines virtuelles
+./vm.sh
